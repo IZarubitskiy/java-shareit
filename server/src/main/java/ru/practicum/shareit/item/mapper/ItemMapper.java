70757ec424fa6d7d2d9de351cb024a8ff9524226
IZ <zarubickiy.igor@gmail.com>
2025-03-03 20:34:16 +0200

    update file from back up

--- server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -17,7 +17,7 @@ public interface ItemMapper {
     @Mapping(target = "owner", source = "owner")
     @Mapping(target = "name", source = "itemDtoRequestCreate.name")
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "request", source = "request")
+    @Mapping(target = "request", source = "itemRequest")
     @Mapping(target = "description", source = "itemDtoRequestCreate.description")
     Item toItemCreate(ItemDtoRequestCreate itemDtoRequestCreate, User owner, ItemRequest itemRequest);
 
