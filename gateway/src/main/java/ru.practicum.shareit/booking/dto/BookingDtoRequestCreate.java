882f2983577b08e3970fc4cc9df6fb3cc5e2f932
IZ <zarubickiy.igor@gmail.com>
2025-03-06 00:01:20 +0200

    add-item-request-and-gateway: items test

--- gateway/src/main/java/ru.practicum.shareit/booking/dto/BookingDtoRequestCreate.java
+++ gateway/src/main/java/ru.practicum.shareit/booking/dto/BookingDtoRequestCreate.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,9 +17,11 @@ import java.time.LocalDateTime;
 public class BookingDtoRequestCreate {
     private Long itemId;
 
+    @NotNull(message = "Start date is required")
     @FutureOrPresent(message = "Start date must be present or in the future")
     private LocalDateTime start;
 
+    @NotNull(message = "End date is required")
     @Future(message = "End date must be in the future")
     private LocalDateTime end;
 }
