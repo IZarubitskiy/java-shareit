b4c6dc624136ea7c006c000aa713c70ed33a7ada
IZ <zarubickiy.igor@gmail.com>
2025-03-05 20:13:11 +0200

    add-item-request-and-gateway: items test

---
+++ server/src/test/java/ru/practicum/shareit/item/mapper/ItemMapperTest.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.item.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import ru.practicum.shareit.item.dto.ItemDtoResponse;
+import ru.practicum.shareit.item.dto.ItemDtoResponseSeek;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapperTest {
+    private final ItemMapper itemMapper = Mappers.getMapper(ItemMapper.class);
+
+    @Test
+    void testNulls() {
+        Item item = itemMapper.toItem(null);
+        item = itemMapper.toItemCreate(null, null, null);
+        item = itemMapper.toItemUpdate(null, null, null);
+        ItemDtoResponse itemResponse = itemMapper.toItemDtoResponse(null);
+        ItemDtoResponse itemResponseComments = itemMapper.toItemDtoResponse(null);
+        ItemDtoResponseSeek itemResponseBookingComments = itemMapper.toItemDtoResponseSeek(null, null, null, null);
+    }
+}
